e00cb81d8ee47ce36a7711827a8274fcfee193b7
Reis567 <matheusdosreis9@gmail.com>
2023-11-13 19:21:48 -0300

    base style of header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import Cabecalho from "@/components/cabecalho/Cabecalho"
 
+const colors = {
+  white: '#f7f2ee',
+  black: '#000000',
+  brown: '#341d08',
+};
+
 export default function Home() {
   return (
       <Cabecalho/>
--- src/components/cabecalho/Cabecalho.tsx
+++ src/components/cabecalho/Cabecalho.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styled from 'styled-components';
 
+const colors = {
+  white: '#f7f2ee',
+  black: '#000000',
+  brown: '#341d08',
+};
 const Cont = styled.div`
-  color: #333;
-  padding: 10px;
-  border: 3px solid #ddd;
+  background: ${colors.brown};
+  padding: 20px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
 `;
+
 const Img = styled.img`
   border-radius:10px;
   max-height:80px;
