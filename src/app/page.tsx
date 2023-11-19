c3bb0798a2eda1175c4753b31a076c36218e1392
Reis567 <matheusdosreis9@gmail.com>
2023-11-18 21:30:44 -0300

    Dialog import from mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client"
-import Lista from "@/components/lista/Lista"
-import { Profissional } from "@/components/entidades/profissional"
-import { useIndex} from "@/hooks/pages/useIndex"
+import Lista from "@/components/lista/Lista";
+import { Dialog, Paper } from "@mui/material";
+import { Profissional } from "@/components/entidades/profissional";
+import { useIndex } from "@/hooks/pages/useIndex";
 
 export default function Home() {
-  const {listaProfissionais} = useIndex();
+  const { listaProfissionais } = useIndex();
+
   return (
-
-
     <div>
       <Lista profissionais={listaProfissionais}></Lista>
+      <Dialog open={true} fullWidth PaperProps={{ sx: { padding: '48px' } }}>
+        test
+      </Dialog>
     </div>
-
-  )
+  );
 }
